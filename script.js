ed829c9035482170e8d1ad53b740c71735fe4a3a
XScripts2025 <yametehahaku123@gmail.com>
2025-03-09 21:15:53 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("copyBtn").addEventListener("click", function() {
-    let scriptBox = document.getElementById("scriptBox");
-    scriptBox.select();
-    scriptBox.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand("copy");
-
-    alert("Script copied to clipboard!");
-});
